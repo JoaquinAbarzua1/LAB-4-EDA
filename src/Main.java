0f5c8bdcc8fa59e6f159b5a361a4de2b8258eedb
Joaquin Abarzua <joaquin.abarzua@mail.udp.cl>
2025-05-29 13:29:55 -0400

    prueba 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+class Paciente{}
+
+class AreaAtencion{}
+
+class Hospital{}
+
+class GeneradorPacientes{}
+
+class SimuladorUrgencia{}
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
